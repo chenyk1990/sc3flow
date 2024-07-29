76e1175e69bd360fe982c93303aa54aab7f6d369
Yangkang Chen <chenyk1990@gmail.com>
2024-07-29 13:21:26 -0500

    Mon Jul 29 13:21:26 CDT 2024

---
+++ demos/test_run_culberson_202211.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Please move the correct parameter file first
+# cp ../pars/ai_picker.inp.culberson_202211 ./ai_picker.inp
+
+~/sc3flow/utils/discontinuous_picker.py -s "2022-11-01 00:00:00" -e "2022-12-01 00:00:00" -n 0.125 
+
+
+
+
+
